68d586060f30dda1afca33a76910722492fe1712
RealMarkSussex <marksussex6@gmail.com>
2022-03-13 22:24:49 +0000

    added set graph

--- lib/models/set_model.dart
+++ lib/models/set_model.dart
@@ -1,9 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class SetModel {
   final String uid;
   final DateTime date;
+  final int numberOfSets;
 
-  SetModel({required this.uid, required this.date});
+  SetModel({required this.uid, required this.date, required this.numberOfSets});
 
   SetModel.fromJson(Map<String, Object?> json)
-      : this(uid: json['uid']! as String, date: json['date']! as DateTime);
+      : this(
+            uid: json['uid']! as String,
+            date: (json['date'] as Timestamp).toDate(),
+            numberOfSets: json["numberOfSets"] as int);
 }
--- lib/pages/statistics_page.dart
+++ lib/pages/statistics_page.dart
@@ -14,16 +14,37 @@ class StatisticsPage extends StatelessWidget {
       body: FutureBuilder<List<SetModel>>(
           future: FirebaseService().getSetsForUser(),
           builder: (context, snapshot) {
-            var data = snapshot.data!;
-
-            var series = [
-              charts.Series(
-                  id: "Sets",
-                  data: data,
-                  domainFn: (setModel, value) => null,
-                  measureFn: (setModel, value) => null)
-            ];
-            return Column();
+            if (snapshot.connectionState == ConnectionState.done) {
+              var data = snapshot.data!;
+              List<charts.Series<dynamic, String>> series = [
+                charts.Series(
+                    id: "Sets",
+                    data: data,
+                    domainFn: (dynamic setModel, _) =>
+                        (setModel.date.toString().substring(0, 11)),
+                    measureFn: (dynamic setModel, _) => setModel.numberOfSets)
+              ];
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    const Text(
+                      "Number of Sets by Day",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 20.0),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: charts.BarChart(series, animate: true),
+                      ),
+                    )
+                  ],
+                ),
+              );
+            } else {
+              return const SizedBox.shrink();
+            }
           }),
     );
   }
